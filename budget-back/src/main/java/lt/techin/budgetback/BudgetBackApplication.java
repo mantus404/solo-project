74566161d04cf1be21ddd12bb522a1363612a4c0
Mantas Upstas <m.upstas@gmail.com>
2023-05-02 18:41:09 +0300

    Budget app front/back init

---
+++ budget-back/src/main/java/lt/techin/budgetback/BudgetBackApplication.java
@@ -0,0 +1,13 @@
+package lt.techin.budgetback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBackApplication.class, args);
+	}
+
+}
---
+++ budget-back/src/test/java/lt/techin/budgetback/BudgetBackApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.budgetback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
